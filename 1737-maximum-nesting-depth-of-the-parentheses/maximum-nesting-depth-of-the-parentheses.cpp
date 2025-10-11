05900dafb5b9fcb5b9c98544ec753fdcda8dcee2
Adhiseem Pandey <164878985+AdhiseemPandey@users.noreply.github.com>
2025-10-11 22:05:10 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (25.26%) - LeetSync

--- 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.cpp
+++ 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.cpp
@@ -1,18 +1,29 @@
 class Solution {
 public:
     int maxDepth(string s) {
-       int ans = 0 , n = 0 ; 
-       stack<char>ap;
-       for (auto ch : s ){
-        if(ch =='('){
-            ap.push(ch);
-        }
-        else if ( ch ==')'){
-            ap.pop();
-        }
-        n = ap.size();
-        ans = ans < n ? n : ans;
-       } 
-       return ans;
+      int ans = 0 ;
+      int depth = 0 ; 
+      for (auto ch : s){
+        if( ch == '(') depth++;
+        else if ( ch == ')') depth--;
+        ans = ans < depth ? depth : ans ;
+      } 
+      return ans ;
     }
 };
+
+
+// //BETTER APPROACH => N & N complexity
+// int ans = 0 , n = 0 ; 
+//        stack<char>ap;
+//        for (auto ch : s ){
+//         if(ch =='('){
+//             ap.push(ch);
+//         }
+//         else if ( ch ==')'){
+//             ap.pop();
+//         }
+//         n = ap.size();
+//         ans = ans < n ? n : ans;
+//        } 
+//        return ans;
