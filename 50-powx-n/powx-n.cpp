b790bde9c4bb3f50d9354d0473c5ec82ba181821
Adhiseem Pandey <164878985+AdhiseemPandey@users.noreply.github.com>
2025-10-28 13:02:31 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (14.66%) - LeetSync

---
+++ 50-powx-n/powx-n.cpp
@@ -0,0 +1,24 @@
+using db = double;
+using ll = long long;
+using I = int;
+
+class Solution {
+public:
+    db myPow(db x, I n) {
+        db ans = 1.0;
+        ll nn = n ;
+        if ( nn < 0 ) nn = -1 * nn ;
+        while(nn){
+            if( nn % 2 ){
+                ans = ans * x ;
+                nn = nn - 1 ;
+            }
+            else {
+                x = x * x ; 
+                nn = nn / 2 ; 
+            }
+        } 
+        if ( n < 0 ) ans = (double)(1.0) / (double)(ans);
+        return ans ;
+    }
+};
