1d2746af05190ce2de22078d3217a01feb1afb74
Adhiseem Pandey <164878985+AdhiseemPandey@users.noreply.github.com>
2025-11-07 22:55:58 +0530

    Time: 3 ms (30.30%) | Memory: 9.9 MB (59.51%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,30 +1,19 @@
 class Solution {
-private:
-    void solve(vector<int> nums, vector<int> output, int index, vector<vector<int> >& ans) {
-        //base case
-        if(index >= nums.size()) {
-            ans.push_back(output);
-            return ;
-        }
-        
-        //exclude
-        solve(nums, output, index+1, ans);
-        
-        //include
-        int element = nums[index];
-        output.push_back(element);
-        solve(nums, output, index+1, ans);
-        
-    }
-    
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        
-        vector<vector<int> > ans;
-        vector<int> output;
-        int index = 0;
-        solve(nums, output, index, ans);
+        int n = nums.size();
+        int sb = 1 << n;  // total subsets = 2^n
+        vector<vector<int>> ans;
+
+        for (int mask = 0; mask < sb; mask++) {
+            vector<int> subset;
+            for (int i = 0; i < n; i++) {
+                if (mask & (1 << i)) {   // check if i-th bit is set
+                    subset.push_back(nums[i]);
+                }
+            }
+            ans.push_back(subset);
+        }
         return ans;
-        
     }
 };
