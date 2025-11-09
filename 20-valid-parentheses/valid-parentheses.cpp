8216185bd02b9a771703aa4af4b2f606ac121bc8
Adhiseem Pandey <164878985+AdhiseemPandey@users.noreply.github.com>
2025-11-09 23:47:33 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (66.19%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,21 +1,45 @@
+// class Solution {
+// public:
+//     bool isValid(string s) {
+//         stack<char> holder;
+//         for (int i = 0; i < s.size(); i++) {
+//             if (s[i] == '(' || s[i] == '{' || s[i] == '[') {
+//                 holder.push(s[i]); 
+//             } else {
+//                 if (holder.empty()) return false;
+//                 char ch = holder.top();
+//                 holder.pop();
+//                 if ((s[i] == ')' && ch != '(') ||
+//                     (s[i] == '}' && ch != '{') ||
+//                     (s[i] == ']' && ch != '[')) {
+//                     return false;
+//                 }
+//             }
+//         }
+//         return holder.empty();
+//     }
+// };
+
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> holder;
-        for (int i = 0; i < s.size(); i++) {
-            if (s[i] == '(' || s[i] == '{' || s[i] == '[') {
-                holder.push(s[i]); 
-            } else {
-                if (holder.empty()) return false;
-                char ch = holder.top();
-                holder.pop();
-                if ((s[i] == ')' && ch != '(') ||
-                    (s[i] == '}' && ch != '{') ||
-                    (s[i] == ']' && ch != '[')) {
+        stack<char>st;
+        for(char x : s){
+            if(x == '(' || x =='{' || x == '[' ){
+                st.push(x);
+            }else if(x == ')' || x == '}'|| x == ']'){
+                
+                if(st.empty())return false;
+                char top = st.top();
+                if((x == ')' && top!= '(')||
+                   (x == '}' && top!= '{')||
+                   (x == ']' && top!= '[')){
                     return false;
-                }
+             }
+                st.pop();
+                
             }
         }
-        return holder.empty();
+       return st.empty();; 
     }
 };
