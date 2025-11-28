93cba3728935c34e4f53ab7fee9b77068369cfec
Adhiseem Pandey <164878985+AdhiseemPandey@users.noreply.github.com>
2025-11-28 20:18:11 +0530

    Time: 27 ms (42.02%) | Memory: 81.4 MB (71.08%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -1,3 +1,37 @@
+// Divide-and-Conquer Idea - Will give TLE in this question but one way is this too
+// class Solution {
+// public:
+//     int calculateArea(vector<int>& heights, int l, int r) {
+//         if (l > r) return 0;
+
+//         // Find index of minimum height in [l, r]
+//         int minIndex = l;
+//         for (int i = l; i <= r; i++) {
+//             if (heights[i] < heights[minIndex]) {
+//                 minIndex = i;
+//             }
+//         }
+
+//         // Case 1: rectangle using the minimum bar
+//         int areaWithMin = heights[minIndex] * (r - l + 1);
+
+//         // Case 2: largest rectangle in left subarray
+//         int leftArea = calculateArea(heights, l, minIndex - 1);
+
+//         // Case 3: largest rectangle in right subarray
+//         int rightArea = calculateArea(heights, minIndex + 1, r);
+
+//         return max({areaWithMin, leftArea, rightArea});
+//     }
+
+//     int largestRectangleArea(vector<int>& heights) {
+//         return calculateArea(heights, 0, heights.size() - 1);
+//     }
+// };
+
+
+
+// Optimized Single-Stack solution 
 using vi = vector<int>;
 class Solution {
 public:
