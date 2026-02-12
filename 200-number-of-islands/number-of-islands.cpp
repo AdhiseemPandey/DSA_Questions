85573cc14547d0b1893d682585879135452aadd4
Adhiseem Pandey <164878985+AdhiseemPandey@users.noreply.github.com>
2026-02-12 23:53:57 +0530

    Time: 24 ms (77.25%) | Memory: 17.2 MB (51.81%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -46,12 +46,61 @@
 
 
 
-// 
+// BFS Soluton 
+// class Solution {
+//     void bfs(int row, int col, vector<vector<int>> &vis, vector<vector<char>> &grid) {
+//         vis[row][col] = 1;
+//         queue<pair<int,int>> q;
+//         q.push({row, col});
+//         int n = grid.size();
+//         int m = grid[0].size();
+
+//         // 4-directional movement
+//         int dRow[] = {-1, 0, 1, 0};
+//         int dCol[] = {0, 1, 0, -1};
+
+//         while (!q.empty()) {
+//             int r = q.front().first;
+//             int c = q.front().second;
+//             q.pop();
+
+//             for (int i = 0; i < 4; i++) {
+//                 int nRow = r + dRow[i];
+//                 int nCol = c + dCol[i];
+//                 if (nRow >= 0 && nRow < n && nCol >= 0 && nCol < m &&
+//                     grid[nRow][nCol] == '1' && !vis[nRow][nCol]) {
+//                     vis[nRow][nCol] = 1;
+//                     q.push({nRow, nCol});
+//                 }
+//             }
+//         }
+//     }
+
+// public:
+//     int numIslands(vector<vector<char>>& grid) {
+//         int n = grid.size();
+//         int m = grid[0].size();
+//         vector<vector<int>> vis(n, vector<int>(m, 0));
+//         int cnt = 0;
+
+//         for (int row = 0; row < n; row++) {
+//             for (int col = 0; col < m; col++) {
+//                 if (!vis[row][col] && grid[row][col] == '1') {
+//                     cnt++;
+//                     bfs(row, col, vis, grid);
+//                 }
+//             }
+//         }
+//         return cnt;
+//     }
+// };
+
+
+
+// DFS - Recursive solution 
 class Solution {
-    void bfs(int row, int col, vector<vector<int>> &vis, vector<vector<char>> &grid) {
+    void dfs(int row, int col, vector<vector<int>> &vis, vector<vector<char>> &grid) {
         vis[row][col] = 1;
-        queue<pair<int,int>> q;
-        q.push({row, col});
         int n = grid.size();
         int m = grid[0].size();
 
@@ -59,19 +108,12 @@ class Solution {
         int dRow[] = {-1, 0, 1, 0};
         int dCol[] = {0, 1, 0, -1};
 
-        while (!q.empty()) {
-            int r = q.front().first;
-            int c = q.front().second;
-            q.pop();
-
-            for (int i = 0; i < 4; i++) {
-                int nRow = r + dRow[i];
-                int nCol = c + dCol[i];
-                if (nRow >= 0 && nRow < n && nCol >= 0 && nCol < m &&
-                    grid[nRow][nCol] == '1' && !vis[nRow][nCol]) {
-                    vis[nRow][nCol] = 1;
-                    q.push({nRow, nCol});
-                }
+        for (int i = 0; i < 4; i++) {
+            int nRow = row + dRow[i];
+            int nCol = col + dCol[i];
+            if (nRow >= 0 && nRow < n && nCol >= 0 && nCol < m &&
+                grid[nRow][nCol] == '1' && !vis[nRow][nCol]) {
+                dfs(nRow, nCol, vis, grid);
             }
         }
     }
@@ -87,7 +129,7 @@ public:
             for (int col = 0; col < m; col++) {
                 if (!vis[row][col] && grid[row][col] == '1') {
                     cnt++;
-                    bfs(row, col, vis, grid);
+                    dfs(row, col, vis, grid);
                 }
             }
         }
