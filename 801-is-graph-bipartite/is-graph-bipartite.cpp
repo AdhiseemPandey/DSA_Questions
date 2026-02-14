887fa593bbd88c9beade094f9a1ffc62aca4fced
Adhiseem Pandey <164878985+AdhiseemPandey@users.noreply.github.com>
2026-02-14 11:31:53 +0530

    Time: 0 ms (100.00%) | Memory: 17.5 MB (53.88%) - LeetSync

---
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -0,0 +1,36 @@
+class Solution {
+private:
+    bool check(int start, vector<vector<int>>& graph, vector<int>& color) {
+        queue<int> q;
+        q.push(start);
+        color[start] = 0;  // Start coloring with 0
+
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+
+            for (auto it : graph[node]) {
+                if (color[it] == -1) {
+                    color[it] = !color[node];  // Assign opposite color
+                    q.push(it);
+                } else if (color[it] == color[node]) {
+                    return false;  // Conflict found
+                }
+            }
+        }
+        return true;
+    }
+
+public:
+    bool isBipartite(vector<vector<int>>& graph) {
+        int V = graph.size();
+        vector<int> color(V, -1);  // Initialize all as uncolored (-1)
+
+        for (int i = 0; i < V; i++) {
+            if (color[i] == -1) {
+                if (!check(i, graph, color)) return false;
+            }
+        }
+        return true;
+    }
+};
