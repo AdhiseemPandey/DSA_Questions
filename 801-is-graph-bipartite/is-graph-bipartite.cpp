ad32cd7d4795c0bff1ba1e486e4569478959e92e
Adhiseem Pandey <164878985+AdhiseemPandey@users.noreply.github.com>
2026-02-14 11:53:25 +0530

    Time: 0 ms (100.00%) | Memory: 17.4 MB (53.88%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -1,3 +1,39 @@
+// DFS Approach 
+
+class Solution {
+private:
+    bool dfs(int node, int col, vector<int>& color, vector<vector<int>>& graph) {
+        color[node] = col;
+
+        for (auto it : graph[node]) {
+            if (color[it] == -1) {
+                if (!dfs(it, !col, color, graph)) return false;
+            } else if (color[it] == col) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+public:
+    bool isBipartite(vector<vector<int>>& graph) {
+        int v = graph.size();
+        vector<int> color(v, -1);
+
+        for (int i = 0; i < v; i++) {
+            if (color[i] == -1) {
+                if (!dfs(i, 0, color, graph)) return false;
+            }
+        }
+        return true;
+    }
+};
+
+
+
+
+// BFS CODE 
+/*
 class Solution {
 private:
     bool check(int start, vector<vector<int>>& graph, vector<int>& color) {
@@ -34,3 +70,5 @@ public:
         return true;
     }
 };
+
+*/
