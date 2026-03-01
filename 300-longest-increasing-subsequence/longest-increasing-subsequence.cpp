e1667d10091814b9293eea4c69d12823712d055f
Adhiseem Pandey <164878985+AdhiseemPandey@users.noreply.github.com>
2026-03-01 13:42:57 +0530

    Time: 3 ms (79.59%) | Memory: 14.2 MB (78.33%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,4 +1,6 @@
-// class Solution {
+// RECURSIVE CODE 
+// 
+//class Solution {
 //     int LIS(int ind, int prev_ind, vector<int>& nums, int n) {
 //         if (ind == n) return 0;
 
@@ -18,27 +20,48 @@
 // };
 
 
-// /*
+// ***************************************************************************************
+//
+// MEMOIZATION 
+// class Solution {
+//     int LIS(int ind, int prev_ind, vector<int>& nums, int n, vector<vector<int>>& dp) {
+//         if (ind == n) return 0;
 
+//         if (dp[ind][prev_ind + 1] != -1) return dp[ind][prev_ind + 1];
+
+//         int len = LIS(ind + 1, prev_ind, nums, n, dp); // NOT TAKE
+
+//         if (prev_ind == -1 || nums[ind] > nums[prev_ind]) { // TAKE
+//             len = max(len, 1 + LIS(ind + 1, ind, nums, n, dp));
+//         }
+
+//         return dp[ind][prev_ind + 1] = len;
+//     }
+
+// public:
+//     int lengthOfLIS(vector<int>& nums) {
+//         int n = nums.size();
+//         vector<vector<int>> dp(n, vector<int>(n + 1, -1));
+//         return LIS(0, -1, nums, n, dp);
+//     }
+// };
+
+
+
+
+// ***************************************************************************************
+
+
+// BINARY SEARCH & STL with Patience sorting tech
 class Solution {
-    int LIS(int ind, int prev_ind, vector<int>& nums, int n, vector<vector<int>>& dp) {
-        if (ind == n) return 0;
-
-        if (dp[ind][prev_ind + 1] != -1) return dp[ind][prev_ind + 1];
-
-        int len = LIS(ind + 1, prev_ind, nums, n, dp); // NOT TAKE
-        
-        if (prev_ind == -1 || nums[ind] > nums[prev_ind]) { // TAKE
-            len = max(len, 1 + LIS(ind + 1, ind, nums, n, dp));
-        }
-
-        return dp[ind][prev_ind + 1] = len;
-    }
-
 public:
     int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(n + 1, -1));
-        return LIS(0, -1, nums, n, dp);
+        vector<int> temp;
+        for (int x : nums) {
+            auto it = lower_bound(temp.begin(), temp.end(), x);
+            if (it == temp.end()) temp.push_back(x);
+            else *it = x;
+        }
+        return temp.size();
     }
 };
