419379d6e8a0dc9648fc0578b89fb1f7990ce6a7
Adhiseem Pandey <164878985+AdhiseemPandey@users.noreply.github.com>
2026-04-30 19:45:45 +0530

    Time: 962 ms (5.26%) | Memory: 39.1 MB (69.74%) - LeetSync

--- 3470-maximum-score-from-grid-operations/maximum-score-from-grid-operations.cpp
+++ 3470-maximum-score-from-grid-operations/maximum-score-from-grid-operations.cpp
@@ -1,67 +1,61 @@
-using vll = std::vector<long long>;
+// https://www.youtube.com/watch?v=f81qWxDkj0Q
 
 class Solution {
 public:
-    long long maximumScore(vector<vector<int>>& grid) {
-        int n = grid[0].size();
-        if (n == 1) {
+    typedef long long ll;
+    int n;
+    ll t[2][101][101];
+
+    ll solve(bool prevTaken, int prevHeight, int col, vector<vector<int>>& grid, vector<vector<ll>>& colPrefSum) {
+        
+        if(col == n) {
             return 0;
         }
+        
+        ll result = 0;
 
-        vector<vector<vll>> dp(n, vector<vll>(n + 1, vll(n + 1, 0)));
-        vector<vll> prevMax(n + 1, vll(n + 1, 0));
-        vector<vll> prevSuffixMax(n + 1, vll(n + 1, 0));
-        vector<vll> colSum(n, vll(n + 1, 0));
+        if(t[prevTaken][prevHeight][col] != -1) {
+            return t[prevTaken][prevHeight][col];
+        } 
 
-        for (int c = 0; c < n; c++) {
-            for (int r = 1; r <= n; r++) {
-                colSum[c][r] = colSum[c][r - 1] + grid[r - 1][c];
+        for(int height = 0; height <= n; height++) {
+            ll prevColScore = 0;
+            ll currColScore = 0;
+
+            if(!prevTaken && col-1 >= 0 && height > prevHeight) {
+                prevColScore += colPrefSum[height][col] - colPrefSum[prevHeight][col];
+            }
+
+            if(prevHeight > height) {
+                currColScore += colPrefSum[prevHeight][col+1] - colPrefSum[height][col+1];
+            }
+
+
+            ll currColScoreTaken    = currColScore + prevColScore + solve(true, height, col+1, grid, colPrefSum);
+            ll currColScoreNotTaken = prevColScore + solve(false, height, col+1, grid, colPrefSum);
+
+            result = max({result, currColScoreTaken, currColScoreNotTaken});
+        }
+
+        return t[prevTaken][prevHeight][col] = result;
+
+    }
+
+    long long maximumScore(vector<vector<int>>& grid) {
+        n = grid.size();
+
+        memset(t, -1, sizeof(t));
+
+
+        vector<vector<ll>> colPrefSum(n + 1, vector<ll>(n + 1, 0));
+
+        for(int col = 1; col <= n; col++) {
+            for(int row = 1; row <= n; row++) {
+                colPrefSum[row][col] = colPrefSum[row-1][col] + grid[row-1][col-1];
             }
         }
 
-        for (int i = 1; i < n; i++) {
-            for (int currH = 0; currH <= n; currH++) {
-                for (int prevH = 0; prevH <= n; prevH++) {
-                    if (currH <= prevH) {
-                        long long extraScore =
-                            colSum[i][prevH] - colSum[i][currH];
-                        dp[i][currH][prevH] =
-                            std::max(dp[i][currH][prevH],
-                                     prevSuffixMax[prevH][0] + extraScore);
-                    } else {
-                        long long extraScore =
-                            colSum[i - 1][currH] - colSum[i - 1][prevH];
-                        dp[i][currH][prevH] = std::max(
-                            {dp[i][currH][prevH], prevSuffixMax[prevH][currH],
-                             prevMax[prevH][currH] + extraScore});
-                    }
-                }
-            }
-
-            for (int currH = 0; currH <= n; currH++) {
-                prevMax[currH][0] = dp[i][currH][0];
-                for (int prevH = 1; prevH <= n; prevH++) {
-                    long long penalty =
-                        (prevH > currH) ? (colSum[i][prevH] - colSum[i][currH])
-                                        : 0;
-                    prevMax[currH][prevH] =
-                        std::max(prevMax[currH][prevH - 1],
-                                 dp[i][currH][prevH] - penalty);
-                }
-
-                prevSuffixMax[currH][n] = dp[i][currH][n];
-                for (int prevH = n - 1; prevH >= 0; prevH--) {
-                    prevSuffixMax[currH][prevH] = std::max(
-                        prevSuffixMax[currH][prevH + 1], dp[i][currH][prevH]);
-                }
-            }
-        }
-
-        long long ans = 0;
-        for (int k = 0; k <= n; k++) {
-            ans = std::max({ans, dp[n - 1][n][k], dp[n - 1][0][k]});
-        }
-
-        return ans;
+        return solve(false, 0, 0, grid, colPrefSum);
     }
 };
+
