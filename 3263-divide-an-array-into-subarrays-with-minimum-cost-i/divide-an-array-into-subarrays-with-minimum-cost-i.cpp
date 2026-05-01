404eee2ca96746eab4cdbaa6fa734eb050277f30
Adhiseem Pandey <164878985+AdhiseemPandey@users.noreply.github.com>
2026-05-01 20:55:50 +0530

    Time: 0 ms (100.00%) | Memory: 30.6 MB (45.76%) - LeetSync

--- 3263-divide-an-array-into-subarrays-with-minimum-cost-i/divide-an-array-into-subarrays-with-minimum-cost-i.cpp
+++ 3263-divide-an-array-into-subarrays-with-minimum-cost-i/divide-an-array-into-subarrays-with-minimum-cost-i.cpp
@@ -27,3 +27,11 @@ public:
     }
 };
 
+// leetcode soluiton 
+// class Solution {
+// public:
+//     int minimumCost(vector<int>& nums) {
+//         sort(nums.begin() + 1, nums.end());
+//         return reduce(nums.begin(), nums.begin() + 3, 0);
+//     }
+// };
