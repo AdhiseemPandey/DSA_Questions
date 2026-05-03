ce909b8639bb1ed66d5bb1354da9e4d9a1896951
Adhiseem Pandey <164878985+AdhiseemPandey@users.noreply.github.com>
2026-05-03 08:47:31 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (97.22%) - LeetSync

---
+++ 812-rotate-string/rotate-string.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        int n = s.size();
+        if (n != goal.size()) return false;
+        
+        for (int k = 0; k < n; k++) {
+            int i = 0;
+            
+            while (i < n && s[(i + k) % n] == goal[i]) {
+                i++;
+            }
+            if (i == n) return true; // full match found
+        }
+        return false;
+    }
+};
