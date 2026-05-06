d211d932262bc5a5e4576a0408d9962d89782d71
Adhiseem Pandey <164878985+AdhiseemPandey@users.noreply.github.com>
2026-05-06 18:20:57 +0530

    Time: 7 ms (64.63%) | Memory: 11.3 MB (90.07%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,30 +1,48 @@
-// TC => O(N)
-// SC => O(256)
+// // TC => O(N)
+// // SC => O(256)
+// class Solution {
+// public:
+//     int lengthOfLongestSubstring(string s) {
+//         // using mmset for performance 
+//         int hash[256];
+//         // mmset works for 0 and -1 
+//         memset(hash,-1,sizeof(hash));
+
+//         int l = 0 ;
+//         int r = 0 ; 
+//         int maxLen = 0 ; 
+//         int n = s.size() ;
+//         while ( r < n ){
+//             if( hash[s[r]] != -1 ){
+//                 if( hash[s[r]] >= l ){
+//                     l = hash[s[r]] + 1 ; 
+//                 } 
+//             }
+//             int len = r - l + 1 ; 
+//             maxLen = max( len, maxLen );
+//             hash[s[r]] = r ; 
+//             r++;
+//         }
+//         return maxLen;
+//     }
+// };
+
+// Approach 2 - Generate substrings and hash on the way TC => O(N^2) & SC => O(256)
+
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        // using mmset for performance 
-        int hash[256];
-        // mmset works for 0 and -1 
-        memset(hash,-1,sizeof(hash));
+        int maxLen = 0;
+        int n = s.size();
 
-        int l = 0 ;
-        int r = 0 ; 
-        int maxLen = 0 ; 
-        int n = s.size() ;
-        while ( r < n ){
-            if( hash[s[r]] != -1 ){
-                if( hash[s[r]] >= l ){
-                    l = hash[s[r]] + 1 ; 
-                } 
+        for (int i = 0; i < n; i++) {
+            int arr[256] = {0}; // reset for each substring
+            for (int j = i; j < n; j++) {
+                if (arr[s[j]] == 1) break; // duplicate found
+                arr[s[j]] = 1;
+                maxLen = max(maxLen, j - i + 1);
             }
-            int len = r - l + 1 ; 
-            maxLen = max( len, maxLen );
-            hash[s[r]] = r ; 
-            r++;
         }
         return maxLen;
     }
 };
-
-// Approach 2 - Generate substrings and hash on the way TC => O(N^2) & SC => O(256)
