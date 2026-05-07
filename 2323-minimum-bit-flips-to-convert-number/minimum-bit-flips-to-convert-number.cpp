ac01d555e3a72232c15329cbb9ef0ce0eb31c921
Adhiseem Pandey <164878985+AdhiseemPandey@users.noreply.github.com>
2026-05-07 13:53:49 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (26.26%) - LeetSync

--- 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
@@ -1,13 +1,27 @@
+// class Solution {
+// public:
+//     int minBitFlips(int start, int goal) {
+//         int ans = start ^ goal ; 
+//         int cnt = 0 ; 
+//         for ( int i = 0 ; i < 31 ; i++ ){
+//             if ( ans & ( 1 << i )){
+//                 cnt += 1 ;
+//             }
+//         }
+//         return cnt ; 
+//     }
+// };
+
+// Brains algo
 class Solution {
 public:
     int minBitFlips(int start, int goal) {
-        int ans = start ^ goal ; 
+        int ans = start ^ goal;
         int cnt = 0 ; 
-        for ( int i = 0 ; i < 31 ; i++ ){
-            if ( ans & ( 1 << i )){
-                cnt += 1 ;
-            }
-        }
-        return cnt ; 
+        while ( ans ){
+            ans = ans & ( ans - 1 );
+            cnt++;
+        } 
+        return cnt;
     }
 };
