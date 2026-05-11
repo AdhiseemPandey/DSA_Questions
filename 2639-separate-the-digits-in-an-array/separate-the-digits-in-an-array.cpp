148c46a03ca60819c8ac0d8ec9f1397608e87aa2
Adhiseem Pandey <164878985+AdhiseemPandey@users.noreply.github.com>
2026-05-11 09:43:26 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (99.01%) - LeetSync

--- 2639-separate-the-digits-in-an-array/separate-the-digits-in-an-array.cpp
+++ 2639-separate-the-digits-in-an-array/separate-the-digits-in-an-array.cpp
@@ -1,18 +1,35 @@
+// class Solution {
+// public:
+//     vector<int> separateDigits(vector<int>& nums) {
+//         vector<int> ans;
+//         for (int i = 0; i < nums.size(); i++) {
+//             int temp = nums[i];
+//             vector<int> digits;
+//             while (temp > 0) {
+//                 digits.push_back(temp % 10);
+//                 temp /= 10;
+//             }
+//             for (int j = digits.size() - 1; j >= 0; j--) {
+//                 ans.push_back(digits[j]);
+//             }
+//         }
+//         return ans;
+//     }
+// };
+
+
 class Solution {
 public:
     vector<int> separateDigits(vector<int>& nums) {
         vector<int> ans;
-        for (int i = 0; i < nums.size(); i++) {
-            int temp = nums[i];
-            vector<int> digits;
-            while (temp > 0) {
-                digits.push_back(temp % 10);
-                temp /= 10;
-            }
-            for (int j = digits.size() - 1; j >= 0; j--) {
-                ans.push_back(digits[j]);
+        for (int i = nums.size() - 1; i >= 0; i--) {
+            int x = nums[i];
+            while (x > 0) {
+                ans.push_back(x % 10);
+                x /= 10;
             }
         }
+        reverse(ans.begin(), ans.end());
         return ans;
     }
 };
