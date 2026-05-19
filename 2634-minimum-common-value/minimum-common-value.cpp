52a42bf0fc485906392a11107e8545ff5d5bd652
Adhiseem Pandey <164878985+AdhiseemPandey@users.noreply.github.com>
2026-05-19 10:14:28 +0530

    Time: 4 ms (31.45%) | Memory: 54.4 MB (81.53%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.cpp
+++ 2634-minimum-common-value/minimum-common-value.cpp
@@ -1,5 +1,20 @@
 class Solution {
 public:
+    int getCommon(vector<int>& nums1, vector<int>& nums2) {
+        int i = 0 , j = 0 ; 
+        while ( i < nums1.size() && j < nums2.size() ){
+            if( nums1[i] == nums2[j] ) return nums1[i];
+            if( nums1[i] < nums2[j] ) i++;
+            else j++;
+        }
+        return -1;
+    }
+};
+
+/*
+
+class Solution {
+public:
     // TC => small log(large array)
     bool bs(int target, vector<int>& nums){
         int left = 0 ; 
@@ -30,3 +45,5 @@ public:
         return -1;
     }
 };
+
+*/
